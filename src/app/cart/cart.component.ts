8989ca8da7eab7d08075f0a05af3af07a8ad2c2b
johnPaslawski <jpaslawski93@gmail.com>
2022-05-08 20:43:50 +0200

    cart and shipping prices page implemented with according components

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { Product } from '../products';
 
 @Component({
   selector: 'app-cart',
@@ -8,7 +9,7 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
 
-items = this.cartService.getItems();
+items: Product[] = this.cartService.getItems();
 
   constructor(private cartService: CartService) { }
 
